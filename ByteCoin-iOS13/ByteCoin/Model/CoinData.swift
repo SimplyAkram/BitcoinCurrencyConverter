a4e565b0958e7ef21fc6fe5bf228b3cdf4d10eaf
Akram Ali <81180304+SimplyAkram@users.noreply.github.com>
2023-09-02 00:16:19 -0500

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Akram Ali on 7/19/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let rate: Double
+    
+}
